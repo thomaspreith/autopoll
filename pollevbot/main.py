24ae090b1a3651082fd71350cbd3cfe995ec482a
thomaspreith <thomaspreith@gmail.com>
2024-08-29 18:28:43 -0500

    Update main.py

--- pollevbot/main.py
+++ pollevbot/main.py
@@ -5,10 +5,11 @@ def main():
     user = 'My Username'
     password = 'My Password'
     host = 'PollEverywhere URL Extension e.g. "uwpsych"'
+    login_type = 'pollev'
 
     # If you're using a non-uw PollEv account,
     # add the argument "login_type='pollev'"
-    with PollBot(user, password, host) as bot:
+    with PollBot(user, password, host, login_type) as bot:
         bot.run()
 
 
